f20cf557bb0e5e9be0543057ed9077645f595d4f
sodaus145 <shinseongjune.dev@gmail.com>
2021-03-18 22:11:52 +0900

    preset

--- Assets/Scripts/Champion.cs
+++ Assets/Scripts/Champion.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChampPreset : MonoBehaviour
+public abstract class Champion : MonoBehaviour
 {
     public float HP { get; set; }
     public float AD { get; set; }
@@ -14,6 +14,34 @@ public class ChampPreset : MonoBehaviour
     public float CritDamage { get; set; }
     public float Reach { get; set; }
     public float MoveSpeed { get; set; }
-    
+    public float Tenacity { get; set; }
 
+    enum State
+    {
+        Idle,
+        Stop,
+        Move,
+        Attack,
+        AttackMove,
+        Skill
+    }
+
+    State state;
+
+    protected abstract void Idle();
+
+    protected abstract void Stop();
+
+    protected abstract void Move();
+
+    protected abstract void Attack();
+
+    protected abstract void AttackMove();
+
+    protected abstract void Skill();
+
+    protected virtual void Start()
+    {
+        state = State.Idle;
+    }
 }
