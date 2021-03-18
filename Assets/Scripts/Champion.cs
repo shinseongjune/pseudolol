dbae7456dd94c26a693ccbc428826a6f60040e15
sodaus145 <shinseongjune.dev@gmail.com>
2021-03-18 18:53:54 +0900

    test2

--- Assets/Scripts/Champion.cs
+++ Assets/Scripts/Champion.cs
@@ -5,16 +5,15 @@ using UnityEngine;
 public class ChampPreset : MonoBehaviour
 {
     public float HP { get; set; }
+    public float AD { get; set; }
+    public float AP { get; set; }
+    public float Defense { get; set; }
+    public float Resist { get; set; }
+    public float AS { get; set; }
+    public float CritChance { get; set; }
+    public float CritDamage { get; set; }
+    public float Reach { get; set; }
+    public float MoveSpeed { get; set; }
+    
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
